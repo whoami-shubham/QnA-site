9a53f3c6f59373c6b2b8b8769d57b07bf18ed826
whoami-shubham <shubhamkumarjha0013@gmail.com>
2019-05-22 16:17:30 +0530

    Updates

---
+++ precache-manifest.cae65fb41fc7c009ada66eb79e06a219.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "67483d73ea62bd64d3a557817fbbe654",
+    "url": "/QnA-site/static/media/geography.67483d73.jpg"
+  },
+  {
+    "revision": "7b5367065e61dab6f8b2",
+    "url": "/QnA-site/static/css/main.497bb3a9.chunk.css"
+  },
+  {
+    "revision": "7198e29ff70c93eacd2e",
+    "url": "/QnA-site/static/js/runtime~main.16356b2b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/QnA-site/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f3c47749e7d51c116211",
+    "url": "/QnA-site/static/js/2.e897627d.chunk.js"
+  },
+  {
+    "revision": "e679eba08464daab0bacbcd8f1632990",
+    "url": "/QnA-site/static/media/loading.e679eba0.gif"
+  },
+  {
+    "revision": "4c303152a88dbfc9cd5d8753ae5612f4",
+    "url": "/QnA-site/static/media/animals.4c303152.jpg"
+  },
+  {
+    "revision": "b8bc951f31695b83a59b2799329b9ea5",
+    "url": "/QnA-site/static/media/space.b8bc951f.jpg"
+  },
+  {
+    "revision": "7b5367065e61dab6f8b2",
+    "url": "/QnA-site/static/js/main.aea535ca.chunk.js"
+  },
+  {
+    "revision": "de429aa0ad7ca2c9789c95582e713ec1",
+    "url": "/QnA-site/static/media/biology.de429aa0.jpg"
+  },
+  {
+    "revision": "de15806ce14938695dcc9a1fbc0c54da",
+    "url": "/QnA-site/static/media/food.de15806c.jpg"
+  },
+  {
+    "revision": "f69e999b5593c80d27b251e96606e7d6",
+    "url": "/QnA-site/static/media/education.f69e999b.jpg"
+  },
+  {
+    "revision": "8394618a5b6214e54f339be1dfdaf673",
+    "url": "/QnA-site/static/media/user1.8394618a.png"
+  },
+  {
+    "revision": "b576b7ca552f0f2a9a0eb26075ba6929",
+    "url": "/QnA-site/static/media/user2.b576b7ca.jpg"
+  },
+  {
+    "revision": "953507ed0751e14e4236b51d69c7152e",
+    "url": "/QnA-site/static/media/user3.953507ed.jpg"
+  },
+  {
+    "revision": "f3c47749e7d51c116211",
+    "url": "/QnA-site/static/css/2.198b8ab0.chunk.css"
+  },
+  {
+    "revision": "affa517260609fad817a20b54d936e0f",
+    "url": "/QnA-site/index.html"
+  }
+];
